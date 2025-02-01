1e02db7f0fc73669edf09a8591bb7cf301abb40f
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-02-01 13:25:17 -0300

    fix: updated styles

--- resources/js/Pages/Tickets/TicketIndex.jsx
+++ resources/js/Pages/Tickets/TicketIndex.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Chatbot from "@/Components/ChatBot/ChatBot";
 import { Link, Head } from "@inertiajs/react";
 
@@ -6,15 +7,25 @@ export default function TicketIndex({ user }) {
         <>
             <Head title="Welcome" />
             <div className="relative flex flex-col sm:flex-row sm:justify-center sm:items-center min-h-screen bg-gray-100 selection:bg-red-500 selection:text-white">
-                <div className="w-full sm:w-1/2 flex items-center justify-center p-6">
+                <motion.div
+                    className="w-full sm:w-1/2 flex items-center justify-center p-6"
+                    initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.8 }}
+                >
                     <img
                         src="https://img.freepik.com/fotos-gratis/ilustracao-3d-de-caneta-colocando-carrapatos-azuis-no-papel_107791-15675.jpg?t=st=1738426309~exp=1738429909~hmac=06fe98af87e0a2c13e2635341979a6c08a9930017dc60059635af5f52f1fd866&w=740"
                         alt="Tickets Support"
                         className="w-full max-w-md rounded-xl shadow-xl transition-transform duration-300 hover:scale-105"
                     />
-                </div>
-
-                <div className="w-full sm:w-1/2 flex flex-col items-center sm:items-start justify-center p-6">
+                </motion.div>
+                
+                <motion.div
+                    className="w-full sm:w-1/2 flex flex-col items-center sm:items-start justify-center p-6"
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.8 }}
+                >
                     <h1 className="text-4xl font-extrabold text-blue-700 mb-6 text-center sm:text-left leading-tight">
                         Olá, {user.name}
                     </h1>
@@ -45,7 +56,7 @@ export default function TicketIndex({ user }) {
                             Visualizar Tickets
                         </Link>
                     </div>
-                </div>
+                </motion.div>
             </div>
             <Chatbot />
         </>
